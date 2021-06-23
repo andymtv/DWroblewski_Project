597a1a6045e5deb8d582a1c8a02e6e2bd21b34c7
andymtv <andy.mtv12@gmail.com>
2021-06-23 20:30:53 +0200

    adapted for devices and resolutions

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,39 @@ document.addEventListener('DOMContentLoaded', () => {
     banner.style.bottom = `-${banner.scrollHeight}px`;
   })
 })
+
+
+
+window.addEventListener('load', function () {
+  const mainContainer = $('.main-wrapper');
+  let scale = 1;
+  function changeSize(el, scale) {
+    if(window.innerWidth >= 1024) {
+      if (el[0].getBoundingClientRect().height + $('.section--2_main-wrapper--2')[0].getBoundingClientRect().height + 30 >= window.innerHeight) {
+        scale = scale - 0.01;
+        console.log(scale);
+        el.css('transform', `scale(${scale})`)
+        changeSize(el, scale);
+      } else {
+        console.log('ok');
+        console.log(el[0].getBoundingClientRect());
+        el.css('margin-top', `-${el[0].getBoundingClientRect().top}px`)
+      }
+    } else {
+      if (el[0].getBoundingClientRect().height >= window.innerHeight) {
+        scale = scale - 0.01;
+        console.log(scale);
+        el.css('transform', `scale(${scale})`)
+        changeSize(el, scale);
+      } else {
+        console.log('ok');
+        console.log(el[0].getBoundingClientRect());
+        el.css('margin-top', `-${el[0].getBoundingClientRect().top}px`)
+      }
+    }
+    
+  }
+
+  changeSize(mainContainer, scale)
+
+});
